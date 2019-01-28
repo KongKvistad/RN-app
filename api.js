665bf0ae1c23a436c6d030e615b5925cd84396e5
KongKvistad <42936783+KongKvistad@users.noreply.github.com>
2019-01-28 12:58:50 +0100

    Add files via upload

--- api.js
+++ api.js
@@ -1,34 +1,58 @@
+
 //mysql setup and test data retrival
 
 let mysql = require('mysql')
 
 let express = require("express");
 
+var bodyParser = require('body-parser')
+
 let app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
-app.get('/products/:id', (req, res) => {
-	console.log('fetching product with serial: ' + req.params.id)
-
-	const connection = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: 'E10trb01',
-		database: 'products'
-	})
-	connection.query("SELECT * FROM productData WHERE serial =" + req.params.id +";", (err, result, rows, fields) => {
-		console.log(result);
-		if (result.length <= 0) {
-			res.send([{serial: "No product found"}]);
-		} else {
-		res.json(result);
-		}
-	})
-	//res.end();
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'E10trb01',
+	database: 'products',
+	
 })
 
 //define listening path for R/N fetch queries
 
 app.listen(process.env.PORT || 3000, () => {
- console.log('Server running on http://localhost:3000')
+	console.log('Server running on http://localhost:3000')
+   })
+
+
+app.get('/products/:id', (req, res) => {
+	console.log('fetching product with serial: ' + req.params.id)
+
+	connection.query("SELECT * FROM productdata WHERE serial ="+ req.params.id + ";", (err, result, rows, fields) => {
+        if (err) {
+			throw err;
+		} else if (result.length <= 0) {
+			res.send([{}]);
+			console.log(result.length)
+		} else {
+			console.log(result);
+			res.send(JSON.stringify({"status":200, "error": null, "response": result}));
+		}
+	})
 })
+
+app.post('/update/:id', (req, res) => {
+	console.log (req.body.firstParam, req.params.id);
+	connection.query("UPDATE `productdata` SET `spend` = 300 WHERE `id` = 1;", (err, result,) => {
+		if (err) {
+			throw err;
+		} else {
+			console.log(result);
+		}
+	})
+
+})
+
+
